7e455e8a4a5bfbf18fa523d638af3668855c2dc7
stewie <stewiemayer@hotmail.com>
2025-08-13 12:19:55 -0600

    chore: se agrega el modo standalone

--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,65 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin =
   require("webpack").container.ModuleFederationPlugin;
 
-module.exports = {
-  entry: "./src/index.tsx",
-  mode: "development",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+module.exports = (env) => {
+  const standaloneMode = env && env.standalone ? true : false;
+
+  return {
+    entry: "./src/index.tsx",
+    mode: "development",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
-      {
-        test: /\.(css|scss)$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-        exclude: /node_modules/,
-      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(css|scss)$/,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "./public/index.html" }),
+      new ModuleFederationPlugin({
+        name: "mfCharacters",
+        filename: "re-mf-characters.js",
+        exposes: {
+          "./MfCharacters": "./src/App",
+        },
+        shared: {
+          react: {
+            singleton: true,
+            requiredVersion: dependencies.react,
+            eager: standaloneMode,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: dependencies["react-dom"],
+            eager: standaloneMode,
+          },
+        },
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "./public/index.html" }),
-    new ModuleFederationPlugin({
-      name: "mfCharacters",
-      filename: "re-mf-characters.js",
-      exposes: {
-        "./MfCharacters": "./src/App",
-      },
-      shared: {
-       react:{singleton:true, requiredVersion: dependencies.react},
-       "react-dom": {singleton:true, requiredVersion:dependencies["react-dom"]}
-      },
-    }),
-  ],
-  devServer: {
-    static: "./dist",
-    port: 3001,
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-  },
+    devServer: {
+      static: "./dist",
+      port: 3001,
+      hot: true,
+      open: standaloneMode,
+      historyApiFallback: true,
+    },
+  };
 };
