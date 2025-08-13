5a7c9c429676662e5bad162eaec364ce84ab6d66
stewie <stewiemayer@hotmail.com>
2025-08-13 12:12:51 -0600

    fix: se limpian dependencias innecesarias

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,6 @@ import type { Config } from "jest";
 
 const config: Config = {
   setupFilesAfterEnv: ["<rootDir>/src/setupTest.ts"],
-  transform: {
-    "^.+\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.json" }],
-    "^.+\.(js|jsx)$": "babel-jest",
-  },
   collectCoverage: true,
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,10 +1,1 @@
 import "@testing-library/jest-dom";
-import { configure } from "@testing-library/react";
-import { TextEncoder, TextDecoder } from 'util';  
-
-configure({ testIdAttribute: "data-test-id" });
-
-// @ts-ignore
-global.TextEncoder = TextEncoder
-// @ts-ignore
-global.TextDecoder = TextDecoder
