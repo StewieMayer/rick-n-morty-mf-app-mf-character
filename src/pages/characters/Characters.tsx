c90b505b2e0df3e4a84944938a2c7ff71d42cba9
stewie <stewiemayer@hotmail.com>
2025-08-14 18:49:46 -0600

    feat: se engloba en un contenedor

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -3,9 +3,11 @@ import CharacterForm from "./components/character-form/CharacterForm";
 
 const Characters: React.FC = () => {
   return (
-    <div className="flex  flex-col justify-center gap-4 h-100">
-      <CharacterForm />
-      <CharacterContainer />
+    <div className="container mx-auto py-4">
+      <div className="flex flex-col items-center gap-4">
+        <CharacterForm />
+        <CharacterContainer />
+      </div>
     </div>
   );
 };
