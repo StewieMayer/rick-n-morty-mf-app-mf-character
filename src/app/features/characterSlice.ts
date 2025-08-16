e75582235edf496d19819a8fa6828e03a6482248
stewie <stewiemayer@hotmail.com>
2025-08-16 13:13:02 -0600

    feat: se agregan controladores del modal al slice

--- src/app/features/characterSlice.ts
+++ src/app/features/characterSlice.ts
@@ -1,4 +1,4 @@
-import { CharactersPageState } from "@/types/characterTypes";
+import { Character, CharactersPageState } from "@/types/characterTypes";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
@@ -7,6 +7,8 @@ const initialState: CharactersPageState = {
   name: undefined,
   species: undefined,
   status: undefined,
+  currentCharacter: null,
+  isModalOpen: false,
 };
 
 export const characterSlice = createSlice({
@@ -25,10 +27,16 @@ export const characterSlice = createSlice({
     setStatus: (state, action: PayloadAction<string>) => {
       state.status = action.payload;
     },
+    setCurrentCharacter: (state, action: PayloadAction<Character | null>) => {
+      state.currentCharacter = action.payload;
+    },
+    setIsModalOpen: (state,action: PayloadAction<boolean>) =>{
+      state.isModalOpen = action.payload;
+    }
   },
 });
 
-export const { setName, setPage, setSpecies, setStatus } =
+export const { setName, setPage, setSpecies, setStatus, setCurrentCharacter, setIsModalOpen } =
   characterSlice.actions;
 export const selectCharacter = (state: RootState): CharactersPageState =>
   state.character;
