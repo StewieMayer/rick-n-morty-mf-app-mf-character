2872eb4cbdb41ae2e33727a5f1f3fdf8ce9f90b5
stewie <stewiemayer@hotmail.com>
2025-08-20 21:52:07 -0600

    feat: se deja toda la logica del Panel en CharacterCard

--- src/pages/characters/components/character-modal/CharacterModal.tsx
+++ src/pages/characters/components/character-modal/CharacterModal.tsx
@@ -1,7 +1,6 @@
 import Modal from "@/components/modal/Modal";
 import { Character } from "@/types/characterTypes";
-import customKey from "@/utils/customKey";
-import { Link } from "react-router-dom";
+import CharacterCard from "../character-card/CharacterCard";
 
 interface CharacterModalProps {
   open: boolean;
@@ -32,36 +31,9 @@ const CharacterModal: React.FC<CharacterModalProps> = ({
       <div className="fixed inset-0 flex w-screen items-center justify-center">
         <Modal.Panel
           transition
-          className="w-md lg:w-3xl bg-white rounded-2xl overflow-hidden box-border-shadow-lg flex flex-col lg:flex-row"
         >
           {character && (
-            <>
-              <div className="relative">
-                <img
-                  src={character.image}
-                  alt={customKey([character.id, character.name])}
-                  className="w-full lg:w-sm"
-                />
-                <Modal.Title className="absolute w-full bottom-0 bg-black/50 text-3xl font-bold text-shadow-blue-500 text-white text-right p-3">
-                  {character.name}
-                </Modal.Title>
-              </div>
-              <div className="p-4 flex flex-col gap-2">
-                <div>
-                  <h3 className="font-bold text-sm text-gray-800">Origen:</h3>
-                  <Link
-                    to="/"
-                    className="hover:text-blue-500 font-semibold text-xs  text-gray-600"
-                  >
-                    {character.origin.name}
-                  </Link>
-                </div>
-                <div>
-                  <h3 className="font-bold text-sm text-gray-800">Genero:</h3>
-                    <span>{character.gender}</span>
-                </div>
-              </div>
-            </>
+              <CharacterCard character={character} size="4xl" full /> 
           )}
         </Modal.Panel>
       </div>
