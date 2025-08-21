4074a4ab8fbcd7868df9774bcec72489292e5a38
stewie <stewiemayer@hotmail.com>
2025-08-20 20:33:25 -0600

    feat: se crea un customhook para character card

---
+++ src/pages/characters/components/character-card/hooks/useCharacterCard.ts
@@ -0,0 +1,54 @@
+import {
+  setCurrentCharacter,
+  setIsModalOpen,
+} from "@/app/features/characterSlice";
+import { useAppDispatch } from "@/app/features/hooks";
+import { Character } from "@/types/characterTypes";
+
+export interface CharacterCardProps {
+  character: Character;
+  size?: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl";
+  full?: boolean;
+}
+
+export const useCharacterCard = ({
+  character,
+  size,
+  full,
+}: CharacterCardProps) => {
+  // Usefull Hooks
+  const dispatch = useAppDispatch();
+
+  // Card Class Constructor
+  const statusClasses: Record<string, string> = {
+    Alive: "bg-green-500",
+    Dead: "bg-red-500",
+    unknown: "bg-gray-300",
+  };
+
+  const baseClass = "rounded-xl overflow-hidden bg-white border-2 border-gray-300 shadow-xl box-border";
+  const flexClass = "flex flex-col md:flex-row"  
+  const sizeClass = `sm:w-100 md:w-full lg:w-${size}`;
+  const hover = "hover:scale-105 hover:cursor-pointer";
+
+  const cardClass = `${baseClass} ${flexClass} ${sizeClass} ${!full && hover}`;
+
+  //Handlers
+  const handleClick = () => {
+    if (full) return;
+    dispatch(setCurrentCharacter(character));
+    dispatch(setIsModalOpen(true));
+  };
+
+  const handleCloseModal = () => {
+    if (!full) return;
+    dispatch(setIsModalOpen(false));
+  };
+
+  return {
+    handleClick,
+    handleCloseModal,
+    statusClass: statusClasses[character.status],
+    cardClass,
+  };
+};
