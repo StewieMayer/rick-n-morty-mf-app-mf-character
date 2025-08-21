71a21c867a7751c5d8c3a5b9a1a641bb7a066157
stewie <stewiemayer@hotmail.com>
2025-08-20 22:10:46 -0600

    fix: se eleva el router fuera de App

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import { store } from "./app/store";
 import React from "react";
 import Characters from "./pages/characters/Characters";
 import "@/styles/index.css";
-import { BrowserRouter } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter basename="/">
-        <Characters />
-      </BrowserRouter>
+      <Characters />
     </Provider>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 const root = ReactDom.createRoot(rootElement!);
 
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename="/">
       <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
