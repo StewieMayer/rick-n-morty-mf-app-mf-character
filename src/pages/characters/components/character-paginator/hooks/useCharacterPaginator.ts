d660c25a466bf29e03394138d22681228d34bcd9
stewie <stewiemayer@hotmail.com>
2025-08-22 08:53:59 -0600

    fix: se corrige la consulta, se agregan memos para habilitar deshabilitar los botones

---
+++ src/pages/characters/components/character-paginator/hooks/useCharacterPaginator.ts
@@ -0,0 +1,84 @@
+import { useGetCharactersMutation } from "@/app/features/charactersApi";
+import {
+  setPage,
+  selectCharacter,
+  setError,
+  setLoading,
+  setCount,
+  setCharacters,
+  setNext,
+  setPrev,
+  setPages,
+} from "@/app/features/characterSlice";
+import { useAppDispatch, useAppSelector } from "@/app/features/hooks";
+import { RootState } from "@/app/store";
+import { useEffect, useMemo, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const useCharacterPaginator = () => {
+  const dispatch = useAppDispatch();
+  const [getCharacters] = useGetCharactersMutation();
+  const [params, setParams] = useSearchParams();
+  const characterState = useAppSelector((state: RootState) =>
+    selectCharacter(state)
+  );
+
+  const { page, pages, next, prev } = characterState;
+  const [currentPage, setCurrentPage] = useState<number>(page);
+
+  const handleNavigation = (page: number) => {
+    if (!page || page < 1 || page > pages) return;
+
+    const apiParams: Record<string, string | number> = { page };
+
+    dispatch(setLoading(true));
+    params.forEach((value, key) => {
+      apiParams[key] = value;
+    });
+
+    getCharacters({ ...apiParams })
+      .unwrap()
+      .then(({ info: { count, pages, next, prev }, results }) => {
+        dispatch(setPage(page));
+        dispatch(setCount(count));
+        dispatch(setPages(pages));
+        dispatch(setNext(next));
+        dispatch(setPrev(prev));
+        dispatch(setCharacters(results));
+      })
+      .catch((error) => dispatch(setError(error.message)))
+      .finally(() => {
+        dispatch(setLoading(false));
+        params.set("page", String(page));
+        setParams(params);
+        setCurrentPage(page);
+      });
+  };
+
+  const handlePrevPage = () => prev && handleNavigation(page - 1);
+  const handleNextPage = () => next && handleNavigation(page + 1);
+  const handleChangePage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPage = parseInt(e.target.value);
+    setCurrentPage(newPage);
+    handleNavigation(newPage);
+  };
+
+  const isPrevDisabled: boolean = useMemo(() => !prev, [prev]);
+  const isNextDisabled: boolean = useMemo(() => !next, [next]);
+
+  useEffect(() => {
+    setCurrentPage(page);
+  }, [page]);
+
+  return {
+    isPrevDisabled,
+    currentPage,
+    pages,
+    isNextDisabled,
+    handlePrevPage,
+    handleChangePage,
+    handleNextPage,
+  };
+};
+
+export default useCharacterPaginator;
