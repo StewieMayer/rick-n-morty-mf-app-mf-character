c74d2f8c2d91efa4162b856a45d3cfdec84b5eb6
stewie <stewiemayer@hotmail.com>
2025-08-23 17:00:53 -0600

    feat: se añade el nuevo breadcrumb al flujo

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -1,11 +1,13 @@
 import CharacterPaginator from "./components/character-paginator/CharacterPaginator";
 import CharacterContainer from "./components/character-container/CharacterContainer";
 import CharacterForm from "./components/character-form/CharacterForm";
+import CharacterBreadcrumb from "./components/character-breadcrumb/CharacterBreadcrumb";
 
 const Characters: React.FC = () => {
   return (
     <div className="flex flex-col w-full">
       <CharacterForm />
+      <CharacterBreadcrumb />
       <div className="container flex-1 mx-auto py-4">
         <div className="flex h-full flex-col items-center gap-4">
           <CharacterContainer />
