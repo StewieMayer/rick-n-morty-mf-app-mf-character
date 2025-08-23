4913a48b1fe657c7de7b58e385c3bf8bf9198bda
stewie <stewiemayer@hotmail.com>
2025-08-23 14:45:32 -0600

    fix: correccion de tamaño

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -4,10 +4,10 @@ import CharacterForm from "./components/character-form/CharacterForm";
 
 const Characters: React.FC = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full">
       <CharacterForm />
-      <div className="container mx-auto py-4">
-        <div className="flex flex-col items-center gap-4">
+      <div className="container flex-1 mx-auto py-4">
+        <div className="flex h-full flex-col items-center gap-4">
           <CharacterContainer />
           <CharacterPaginator />
         </div>
