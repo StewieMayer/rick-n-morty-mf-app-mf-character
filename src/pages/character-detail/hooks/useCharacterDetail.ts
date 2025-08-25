00300e5a0dd38c8094406560c356517736ad3446
stewie <stewiemayer@hotmail.com>
2025-08-24 22:50:54 -0600

    feat: se agregan navegaciones

--- src/pages/character-detail/hooks/useCharacterDetail.ts
+++ src/pages/character-detail/hooks/useCharacterDetail.ts
@@ -1,15 +1,19 @@
 import { useGetCharacterDetailQuery } from "@/app/features/characterDetailApi";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const useCharacterDetail = () => {
   const { id } = useParams();
   const { data: character, isLoading, isError, error } = useGetCharacterDetailQuery(id!);
+
+  const navigate = useNavigate();
+  const backToCharacters = () => navigate('../');
 
   return {
     isLoading,
     isError,
     error,
     character,
+    backToCharacters,
   };
 };
 
